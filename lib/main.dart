620162d515ee049a6e2617ac62566eea7d3128d3
Luis Angel <luis9925.la@gmail.com>
2023-09-09 11:54:13 -0600

    feat: agrega app con cabecera y titulo

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:todo_app/constants/colors.dart';
+import 'package:todo_app/screens/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'To Do App',
+      home: Home(),
+    );
+  }
+}
