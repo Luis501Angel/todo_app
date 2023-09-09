85c8f244459102c8785aa86a84da46159800219b
Luis Angel <luis9925.la@gmail.com>
2023-09-09 13:02:05 -0600

    agrega caja de texto y boton para agregar tareas

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -4,7 +4,6 @@ import '../constants/colors.dart';
 import '../widgets/todo_item.dart';
 
 class Home extends StatelessWidget {
-
   Home({Key? key}) : super(key: key);
 
   final todoList = ToDo.todoList();
@@ -14,30 +13,76 @@ class Home extends StatelessWidget {
     return Scaffold(
         backgroundColor: tdBgColor,
         appBar: _buildAppBar(),
-        body: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-          child: Column(
-            children: [
-              searchBox(),
-              Expanded(
-                child: ListView(
-                  children: [
-                    Container(
-                      margin: const EdgeInsets.only(top: 50, bottom: 20),
+        body: Stack(
+          children: [
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+              child: Column(
+                children: [
+                  searchBox(),
+                  Expanded(
+                    child: ListView(
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.only(top: 50, bottom: 20),
+                          child: const Text(
+                            'All ToDos',
+                            style: TextStyle(
+                                fontSize: 30, fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                        for (ToDo item in todoList)
+                          ToDoItem(
+                            toDo: item,
+                          ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Row(
+                children: [
+                  Expanded(
+                      child: Container(
+                    margin:
+                        const EdgeInsets.only(bottom: 20, right: 20, left: 20),
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                    decoration:
+                        const BoxDecoration(color: Colors.white, boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey,
+                          offset: Offset(0.0, 0.0),
+                          blurRadius: 10.0,
+                          spreadRadius: 0.0)
+                    ]),
+                    child: const TextField(
+                        decoration: InputDecoration(
+                            hintText: 'Add a new item',
+                            border: InputBorder.none)),
+                  )),
+                  Container(
+                    margin: const EdgeInsets.only(bottom: 20, right: 20),
+                    child: ElevatedButton(
                       child: const Text(
-                        'All ToDos',
-                        style: TextStyle(
-                            fontSize: 30, fontWeight: FontWeight.w500),
+                        '+',
+                        style: TextStyle(fontSize: 40),
+                      ),
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        primary: tdBlue,
+                        minimumSize: Size(60, 60),
+                        elevation: 10
                       ),
                     ),
-
-                    for (ToDo item in todoList) 
-                      ToDoItem(toDo: item,),
-                  ],
-                ),
-              )
-            ],
-          ),
+                  )
+                ],
+              ),
+            )
+          ],
         ));
   }
 
